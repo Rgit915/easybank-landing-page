e2c86745be016c408346f3d797c5b220ceb0103d
Rgit915 <rora.goitom@gmail.com>
2025-05-04 00:10:11 -0400

    Structure main layout with core components

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Articles from "./components/Articles";
+import Easybank from "./components/Easybank";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+import "./index.css";
+
+const App = () => {
+  return (
+    <main role="main">
+      <Header />
+      <Hero />
+      <Easybank />
+      <Articles />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
